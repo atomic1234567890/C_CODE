ddfa78f93d2a37e1234a6d9582116004dc04cad8
xyz <2963206943@qq.com>
2025-02-11 17:26:58 +0800

    猜数字

--- test1.c
+++ test1.c
@@ -1,10 +1,53 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void menu()
+{
+	printf("请开始游戏。。。。\n");
+	printf("输入：1，进入\n");
+	printf("输入：0，退出\n");
+}
+void game()
+{
+	int a = 0;
+	int put = 0;
+	a = rand()%100+1;
+	do {
+		printf("请输入你的猜测\n");
+		srand((unsigned int)time(NULL));
+		scanf("%d",&put);
+		if (put > a)
+			printf("输大了\n");
+		else if (put < a)
+			printf("输小了\n");
+		else
+		{
+			printf("正确\n");
+			break;
+		}
+	} while (1);
+}
 int main()
 {
-	int a = 5;
-	
-	printf("%d", a--);
-	printf("%d", a);
+	int elect;
+	do {
+		menu();
+		
+		scanf("%d", &elect);
+		switch(elect)
+		{
+		case 1:
+			printf("欢迎游戏！！！\n");
+		game();
+			break;
+		case 0: {
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("输入错误\n");
+			break;
+		}
+	} while (elect);
 	return 0;
 }
